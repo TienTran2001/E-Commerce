acc81a1dcb49a0eca394208639794290ec35397a
Tiến Trần <tienco201@gmail.com>
2023-04-22 20:16:43 +0700

    #5 ratings

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -23,11 +23,56 @@ const getProduct = asyncHandler(async (req, res) => {
   });
 });
 
-const getAllProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  return res.status(200).json({
-    success: products ? true : false,
-    productsData: products ? products : 'product not found',
+//Filtering, sorting and pagination
+const getProducts = asyncHandler(async (req, res) => {
+  const queries = { ...req.query };
+  // tách các trường đặc biệt ra khỏi query
+  const excludeFields = ['limit', 'sort', 'page', 'fields'];
+  excludeFields.forEach((el) => delete queries[el]);
+
+  // Format lại các operators cho đúng  cú pháp của mongoose
+  let queryString = JSON.stringify(queries);
+  queryString = queryString.replace(
+    /\b(gte|gt|lte|lt)\b/g,
+    (matchedEl) => `$${matchedEl}`
+  );
+  const formatQueries = JSON.parse(queryString);
+
+  // 1. Filtering
+  if (queries?.title)
+    formatQueries.title = { $regex: queries.title, $options: 'i' };
+  let queryCommand = Product.find(formatQueries);
+
+  // 2. Sorting
+  if (req.query.sort) {
+    // abc,kjh => split => ['abc', 'kjh'] => join => abc kjh
+    const sortBy = req.query.sort.split(',').join(' ');
+    queryCommand = queryCommand.sort(sortBy);
+  }
+
+  // 3. field Limiting
+  // title,price => ['title', 'price'] => title price
+  if (req.query.fields) {
+    const fields = req.query.fields.split(',').join(' ');
+    queryCommand.select(fields);
+  }
+
+  // 4. Pagination
+  // page=2&limit=10, 1-10 page 1, 11-20 page 2, 21-30 page 3
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 3;
+  const skip = (page - 1) * limit;
+  queryCommand = queryCommand.skip(skip).limit(limit);
+
+  // execute query
+  queryCommand.exec(async (err, response) => {
+    if (err) throw new Error(err.message);
+    const counts = await Product.find(formatQueries).countDocuments();
+    return res.status(200).json({
+      success: response ? true : false,
+      products: response ? response : 'product not found',
+      counts,
+    });
   });
 });
 
@@ -43,9 +88,63 @@ const updateProduct = asyncHandler(async (req, res) => {
   });
 });
 
+const deleteProduct = asyncHandler(async (req, res) => {
+  const { pid } = req.params;
+  const response = await Product.findByIdAndDelete(pid);
+  return res.status(200).json({
+    success: response ? true : false,
+    message: response
+      ? `Deleted product name: ${response.title}`
+      : `No product found`,
+  });
+});
+
+const ratings = asyncHandler(async (req, res) => {
+  const { _id } = req.user;
+  const { star, comment, pid } = req.body;
+  if (!star || !pid) throw new Error('Missing inputs');
+  const ratingProduct = await Product.findById(pid);
+  const alreadyRating = ratingProduct?.ratings?.find(
+    (el) => el.postedBy.toString() === _id
+  );
+  // console.log(!!alreadyRating);
+  if (alreadyRating) {
+    // update star
+    await Product.updateOne(
+      {
+        ratings: { $elemMatch: alreadyRating },
+      },
+      {
+        $set: {
+          'ratings.$.star': star,
+          'ratings.$.comment': comment,
+        },
+      },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: true,
+    });
+  } else {
+    // add star & comment
+    const response = await Product.findByIdAndUpdate(
+      pid,
+      {
+        $push: { ratings: { star, comment, postedBy: _id } },
+      },
+      { new: true }
+    );
+    return res.status(200).json({
+      success: true,
+    });
+  }
+});
+
 module.exports = {
   createProduct,
   getProduct,
-  getAllProducts,
+  getProducts,
   updateProduct,
+  deleteProduct,
+  ratings,
 };
--- server/routes/product.js
+++ server/routes/product.js
@@ -3,8 +3,11 @@ const controller = require('../controllers/product');
 const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken');
 
 router.post('/', [verifyAccessToken, isAdmin], controller.createProduct);
+router.get('/', controller.getProducts);
+router.put('/ratings', [verifyAccessToken], controller.ratings);
+
+router.delete('/:pid', [verifyAccessToken, isAdmin], controller.deleteProduct);
 router.get('/:pid', controller.getProduct);
-router.get('/', controller.getAllProducts);
 router.put('/:pid', [verifyAccessToken, isAdmin], controller.updateProduct);
 
 module.exports = router;
